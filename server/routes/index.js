1c31862bb97de97f0c115b3306c5993f2e4455e8
aimeedyki <akuwudike.a@gmail.com>
2020-04-08 11:21:27 +0100

    set user permissions according to roles (#3)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
+
 import {
   authenticationController,
   usersController
 } from '../controllers';
+import authentication from '../middlewares/authentication';
 
 const app = express();
 
 // route for creating user
-app.post('/user', usersController.createUser);
+app.post(
+  '/user',
+  authentication.verifyUser,
+  authentication.verifyAdmin,
+  usersController.createUser
+);
 
 // route for signing in user
 app.post('/signin', authenticationController.signinUser);
